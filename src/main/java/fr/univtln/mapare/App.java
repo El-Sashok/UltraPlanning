94894b1d34db7e3af140a6e3636d8c6a33c4a6dc
Sasha <reaubourg.alex@gmail.com>
2020-11-27 12:03:59 +0100

    initial commit

---
+++ src/main/java/fr/univtln/mapare/App.java
@@ -0,0 +1,13 @@
+package fr.univtln.mapare;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
