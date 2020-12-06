4bdeaf84e6ba245b64261de9ea28f96e32710ba8
Sasha <reaubourg.alex@gmail.com>
2020-12-06 19:52:02 +0100

    adds rememberMe checkbox

--- src/main/java/fr/univtln/mapare/gui/Launcher.java
+++ src/main/java/fr/univtln/mapare/gui/Launcher.java
@@ -8,12 +8,15 @@ public class Launcher extends JFrame {
     private JTextField emailTextField;
     private JLabel email;
     private JLabel password;
+    private JButton submit;
+    private JCheckBox rememberMe;
 
     public Launcher(){
         setTitle("Login");
-        setSize(300, 100);
+        setSize(300, 160);
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         add(rootPanel);
+        password.setText("test");
     }
 }
