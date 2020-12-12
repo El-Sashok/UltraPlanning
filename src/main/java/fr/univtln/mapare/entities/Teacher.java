4d69e664713f6283786cac23f2e991de914ce578
Nico <nico.marguerit@gmail.com>
2020-12-12 16:24:07 +0100

    creates Teacher

---
+++ src/main/java/fr/univtln/mapare/entities/Teacher.java
@@ -0,0 +1,34 @@
+package fr.univtln.mapare.entities;
+
+import java.util.Date;
+
+public class Teacher extends Person {
+    private String laboratory;
+    private Role type;
+
+    public Teacher(String lastName, String firstName, Date birthdate, String laboratory, Role type) {
+        super(lastName, firstName, birthdate);
+        this.laboratory = laboratory;
+        this.type = type;
+    }
+
+    public String getLaboratory() {
+        return laboratory;
+    }
+
+    public void setLaboratory(String laboratory) {
+        this.laboratory = laboratory;
+    }
+
+    public Role getType() {
+        return type;
+    }
+
+    public void setType(Role type) {
+        this.type = type;
+    }
+
+    public enum Role {
+        LECTURER, PROFESSOR, ADJUNCT_PROF
+    }
+}
