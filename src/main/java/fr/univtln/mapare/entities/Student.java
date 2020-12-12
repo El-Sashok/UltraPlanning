13708ab1b3381eba64052f4c93a7260766806610
Nico <nico.marguerit@gmail.com>
2020-12-12 16:24:24 +0100

    creates Student

---
+++ src/main/java/fr/univtln/mapare/entities/Student.java
@@ -0,0 +1,9 @@
+package fr.univtln.mapare.entities;
+
+import java.util.Date;
+
+public class Student extends Person{
+    public Student(String lastName, String firstName, Date birthdate) {
+        super(lastName, firstName, birthdate);
+    }
+}
