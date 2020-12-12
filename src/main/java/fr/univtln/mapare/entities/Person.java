044e8e032f60b47b98d166477bbdd3d88de5ec37
Nico <nico.marguerit@gmail.com>
2020-12-12 16:18:43 +0100

    implements Comparable

--- src/main/java/fr/univtln/mapare/entities/Person.java
+++ src/main/java/fr/univtln/mapare/entities/Person.java
@@ -1,9 +1,10 @@
 package fr.univtln.mapare.entities;
 
 import java.util.Date;
+import java.util.Objects;
 
-public class Person {
-    private long id = -1;
+public abstract class Person implements Comparable<Person> {
+    private int id = -1;
     private final String lastName;
     private final  String firstName;
     private final Date birthdate;
@@ -18,7 +19,7 @@ public class Person {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(int id) {
         this.id = id;
     }
 
@@ -32,5 +33,31 @@ public class Person {
 
     public Date getBirthdate() {
         return birthdate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return id - o.id;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "lastName='" + lastName + '\'' +
+                ", firstName='" + firstName + '\'' +
+                '}';
     }
 }
