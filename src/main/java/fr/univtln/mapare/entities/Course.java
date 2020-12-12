25f9e10f89c32a0de318f0fa439a0396cdad47ef
Nico <nico.marguerit@gmail.com>
2020-12-12 18:26:37 +0100

    creates Course

---
+++ src/main/java/fr/univtln/mapare/entities/Course.java
@@ -0,0 +1,29 @@
+package fr.univtln.mapare.entities;
+
+public class Course {
+    private String label;
+    private int nbHour;
+
+    //Constructors
+    public Course(String label, int nbHour) {
+        this.label = label;
+        this.nbHour = nbHour;
+    }
+
+    //Getters & Setters
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public int getNbHour() {
+        return nbHour;
+    }
+
+    public void setNbHour(int nbHour) {
+        this.nbHour = nbHour;
+    }
+}
