b5ff694ac0a82053e133a5758cbbda0c0ba72c10
Nico <nico.marguerit@gmail.com>
2020-12-17 19:21:28 +0100

    adds RoomDAO

---
+++ src/main/java/fr/univtln/mapare/daos/RoomDAO.java
@@ -0,0 +1,44 @@
+package fr.univtln.mapare.daos;
+
+import fr.univtln.mapare.entities.Room;
+import fr.univtln.mapare.entities.Student;
+import fr.univtln.mapare.exceptions.DataAccessException;
+import lombok.extern.java.Log;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Log
+public class RoomDAO extends AbstractDAO<Room> {
+
+    public RoomDAO() throws DataAccessException {
+        super("",
+                "");
+    }
+
+    @Override
+    protected Room fromResultSet(ResultSet resultSet) throws SQLException {
+        return new Room(resultSet.getInt("ID"),
+                resultSet.getString("BUILDING"),
+                resultSet.getInt("NUMBER"),
+                resultSet.getInt("CAPACITY"),
+                resultSet.getString("TITLE"),
+                resultSet.getString("INFO"));
+
+    }
+
+    @Override
+    public Room persist(Room room) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void update(Room room) throws DataAccessException {
+
+    }
+
+    @Override
+    public String getTableName() {
+        return "ROOM";
+    }
+}
