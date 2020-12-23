f4b2bd6ddc3bc9b5283239ce2292e1ac2f3d4269
onilateur <francois-palma@etud.univ-tln.fr>
2020-12-23 15:56:07 +0100

    Adds new GUI form for teacher list selection

---
+++ src/main/java/fr/univtln/mapare/gui/TeacherListSelector.java
@@ -0,0 +1,13 @@
+package fr.univtln.mapare.gui;
+
+import javax.swing.*;
+
+public class TeacherListSelector {
+    private JPanel panel1;
+    private JPanel panel2;
+    private JButton retirerDeLaListeButton;
+    private JButton ajouterEnseignantButton;
+    private JComboBox comboBox1;
+    private JScrollPane scrollPane1;
+    private JList list1;
+}
