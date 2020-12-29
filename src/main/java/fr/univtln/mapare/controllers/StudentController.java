f96902017a883184a1ef4c5113581fc85ee0252c
Nico <nico.marguerit@gmail.com>
2020-12-29 17:24:59 +0100

    adds StudentController

---
+++ src/main/java/fr/univtln/mapare/controllers/StudentController.java
@@ -0,0 +1,23 @@
+package fr.univtln.mapare.controllers;
+
+import fr.univtln.mapare.daos.StudentDAO;
+import fr.univtln.mapare.entities.Student;
+
+import java.sql.SQLException;
+
+public abstract class StudentController {
+
+    public static void loadStudents(Student student) throws SQLException {
+        StudentDAO studentDAO = new StudentDAO();
+        studentDAO.findAll();
+        studentDAO.close();
+    }
+
+    public static void remove(Student student) throws SQLException {
+        StudentDAO studentDAO = new StudentDAO();
+        studentDAO.remove(student.getId());
+        studentDAO.close();
+        Student.popStudentInList(student);
+    }
+
+}
