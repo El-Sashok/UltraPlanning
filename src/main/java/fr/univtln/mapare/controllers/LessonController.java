478e877c7312ecd88fe900985998ae200dd3ad12
Sasha <reaubourg.alex@gmail.com>
2020-12-30 17:00:25 +0100

    finish createLesson

--- src/main/java/fr/univtln/mapare/controllers/LessonController.java
+++ src/main/java/fr/univtln/mapare/controllers/LessonController.java
@@ -2,6 +2,8 @@ package fr.univtln.mapare.controllers;
 
 import fr.univtln.mapare.daos.LessonDAO;
 import fr.univtln.mapare.entities.*;
+import fr.univtln.mapare.entities.Module;
+import fr.univtln.mapare.exceptions.TimeBreakExceptions.*;
 
 import java.sql.SQLException;
 import java.time.LocalDateTime;
@@ -10,6 +12,30 @@ import java.util.List;
 public abstract class LessonController {
 
     public static void createLesson(LocalDateTime startDate, LocalDateTime endDate, String label, String memo, Reservation.State state, Room room, Lesson.Type type, List<Module> modules, List<Group> groups, List<Teacher> managers) throws SQLException {
+
+        for (Reservation r : Reservation.getReservationList()){
+            if (r.getState() == Reservation.State.NP) {
+                if (checkTimeBreak(r.getStartDate(), r.getEndDate(), startDate, endDate)){
+                    for (Teacher dbTeacher : r.getManagers())
+                        for (Teacher LocalTeacher : managers)
+                            if (dbTeacher.getId() == LocalTeacher.getId())
+                                throw new ManagerTimeBreakException(LocalTeacher);
+                    if (r.getRoom().getId() == room.getId())
+                        throw new RoomTimeBreakException(room);
+                    else if (r instanceof Lesson)
+                        for (Group dbGroup : ((Lesson) r).getGroups())
+                            for (Group LocalGroup : groups)
+                                if (dbGroup.getId() == LocalGroup.getId())
+                                    throw new GroupTimeBreakException(LocalGroup);
+                    else if (r instanceof Defence)
+                        for (Group g : groups)
+                            for (Student s : g.getStudents())
+                                if (s.getId() == ((Defence) r).getStudent().getId())
+                                    throw new StudentTimeBreakException(s);
+                }
+            }
+        }
+
         Lesson lesson = new Lesson(-1, startDate, endDate, label, memo, state, room, type);
         for (Module m : modules) lesson.addModule(m);
         for (Group g : groups) lesson.addGroup(g);
@@ -17,5 +43,11 @@ public abstract class LessonController {
         LessonDAO lessonDAO = new LessonDAO();
         lessonDAO.persist(lesson);
         lessonDAO.close();
+    }
+
+    private static boolean checkTimeBreak(LocalDateTime dbStart, LocalDateTime dbEnd, LocalDateTime localStart, LocalDateTime localEnd) {
+        return (dbStart.isAfter(localStart) && dbStart.isBefore(localEnd)) ||
+                (dbEnd.isAfter(localStart) && dbEnd.isBefore(localEnd)) ||
+                (dbStart.isBefore(localStart) && dbEnd.isAfter(localEnd));
     }
 }
