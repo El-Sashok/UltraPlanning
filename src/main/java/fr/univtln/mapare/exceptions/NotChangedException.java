84c302f59da373bcad1330f51de4efcf50dece9e
Nico <nico.marguerit@gmail.com>
2021-01-12 18:47:42 +0100

    creates UpdateException package

--- src/main/java/fr/univtln/mapare/exceptions/NotChangedException.java
+++
@@ -1,9 +0,0 @@
-package fr.univtln.mapare.exceptions;
-
-import fr.univtln.mapare.entities.Entity;
-
-public class NotChangedException extends RuntimeException {
-    public NotChangedException(Entity e) {
-        super("Entity \"" + e + "\" not changed");
-    }
-}
