ae6d3623db80d4d77e14e194b2a9ec57996ee09e
Nico <nico.marguerit@gmail.com>
2021-01-14 15:49:58 +0100

    adds change methods & fixes java doc

--- src/main/java/fr/univtln/mapare/controllers/ModuleController.java
+++ src/main/java/fr/univtln/mapare/controllers/ModuleController.java
@@ -2,6 +2,8 @@ package fr.univtln.mapare.controllers;
 
 import fr.univtln.mapare.daos.ModuleDAO;
 import fr.univtln.mapare.entities.Module;
+import fr.univtln.mapare.exceptions.updateexceptions.EmptyAttributeException;
+import fr.univtln.mapare.exceptions.updateexceptions.NotChangedException;
 
 
 import java.sql.SQLException;
@@ -33,7 +35,7 @@ public abstract class ModuleController {
     }
 
     /**
-     * Permet de Créer un module
+     * Permet de créer un module
      * @param label Nom du module
      * @param nbHour Nombre d'heures du module
      * @throws SQLException Exception SQL
@@ -42,6 +44,43 @@ public abstract class ModuleController {
         Module module = new Module(-1, label, nbHour);
         try (ModuleDAO moduleDAO = new ModuleDAO()) {
             moduleDAO.persist(module);
+        }
+    }
+
+    /**
+     * Permet de changer le nom d'un module
+     * @param module Le module
+     * @param label Nom du module
+     * @throws SQLException Exception SQL
+     * @throws EmptyAttributeException label est vide
+     * @throws NotChangedException Aucune modification apportée
+     */
+    public static void changeLabel(Module module, String label) throws SQLException, EmptyAttributeException, NotChangedException {
+        if (label.isEmpty())
+            throw new EmptyAttributeException("changeLabel", module);
+        if (module.getLabel().equals(label))
+            throw new NotChangedException(module);
+
+        module.setLabel(label);
+        try (ModuleDAO moduleDAO = new ModuleDAO()) {
+            moduleDAO.update(module);
+        }
+    }
+
+    /**
+     * Permet de changer le nombre d'heures d'un module
+     * @param module Le module
+     * @param nbHour Le nombre d'heures du module
+     * @throws SQLException Exception SQL
+     * @throws NotChangedException Aucune modification apportée
+     */
+    public static void changeHours(Module module, int nbHour) throws SQLException, NotChangedException {
+        if (module.getNbHour() == nbHour)
+            throw new NotChangedException(module);
+
+        module.setNbHour(nbHour);
+        try (ModuleDAO moduleDAO = new ModuleDAO()) {
+            moduleDAO.update(module);
         }
     }
 }
